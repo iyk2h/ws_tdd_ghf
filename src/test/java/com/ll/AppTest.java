1ce14dac917ba748f66437d5bfb1a3d8d53be040
youngkyunIm <yee0230@naver.com>
2023-04-03 16:17:31 +0900

    출력을 모니터에 하지 않고 문자열로 얻기

--- src/test/java/com/ll/AppTest.java
+++ src/test/java/com/ll/AppTest.java
@@ -3,7 +3,9 @@ package com.ll;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
+import java.util.SortedMap;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +18,19 @@ class AppTest {
 
         String cmd = sc.nextLine().trim();
         assertThat(cmd).isEqualTo("안녕");
+    }
+
+    @Test
+    @DisplayName("출력을 모니터에 하지 않고 문자열로 얻기")
+    public void t2() {
+        // System.out 에 대한 화면출력 금지 시작
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.print("안녕");
+        String rs = output.toString();
+
+        // System.out 에 대한 화면출력 금지 끝
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs).isEqualTo("안녕");
     }
 }
--- src/test/java/com/ll/TestUtil.java
+++ src/test/java/com/ll/TestUtil.java
@@ -1,7 +1,13 @@
 package com.ll;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TestUtil {
@@ -10,5 +16,21 @@ public class TestUtil {
         InputStream in = new ByteArrayInputStream(input.getBytes());
 
         return new Scanner(in);
+    }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
