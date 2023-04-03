354c477873a8471371d72f77b8dccab8534a2956
youngkyunIm <yee0230@naver.com>
2023-04-03 16:40:45 +0900

    종료 기능 구현, 잘못 입력된 명령어 처리

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,8 +1,10 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new App(new Scanner(System.in)).run();
     }
 }
--- src/test/java/com/ll/AppTest.java
+++ src/test/java/com/ll/AppTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 
 class AppTest {
 
+    //TestUtil
     @Test
     @DisplayName("스캐너에 키보드가 아닌 문자열을 입력")
     public void t1() {
@@ -32,5 +33,41 @@ class AppTest {
         TestUtil.clearSetOutToByteArray(output);
 
         assertThat(rs).isEqualTo("안녕");
+    }
+
+    @Test
+    @DisplayName("프로그램 시작시 타이틀 출력 그리고 종료")
+    public void t3() {
+        Scanner sc = TestUtil.genScanner("종료");
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs)
+                .contains("== 명언 앱 ==")
+                .contains("명령)")
+                .contains("프로그램이 종료되었습니다.")
+                .doesNotContain("올바르지 않은 명령입니다.");
+    }
+
+    @Test
+    @DisplayName("잘못된 명령어 입력에 대학 처리")
+    public void t4() {
+        Scanner sc = TestUtil.genScanner(" 종료2 \n 종료 ".stripLeading().trim());
+
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs)
+                .contains("== 명언 앱 ==")
+                .contains("명령)")
+                .contains("올바르지 않은 명령입니다.");
     }
 }
